d4fd6d2d011af3f9a035d1eb7a2170d0e73eef4a
slark1995 <610158299@qq.com>
2019-10-29 14:40:18 -0600

    Updates

---
+++ precache-manifest.352d9341fe4fe26b58863554e30f122e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100d280198f35fd4a8db181dcf7e248b",
+    "url": "/React-myblog/index.html"
+  },
+  {
+    "revision": "b93774dc64fb776b31d2",
+    "url": "/React-myblog/static/css/main.cb936623.chunk.css"
+  },
+  {
+    "revision": "c191a8c69a9b97d30be5",
+    "url": "/React-myblog/static/js/2.f2181278.chunk.js"
+  },
+  {
+    "revision": "b93774dc64fb776b31d2",
+    "url": "/React-myblog/static/js/main.a025aebd.chunk.js"
+  },
+  {
+    "revision": "0c21d3d6676429290760",
+    "url": "/React-myblog/static/js/runtime-main.40fe5479.js"
+  },
+  {
+    "revision": "6238802ceb77716735e7dc57941e0e02",
+    "url": "/React-myblog/static/media/democard.6238802c.png"
+  },
+  {
+    "revision": "e2c24fb036514f40548b89b40ff18fe6",
+    "url": "/React-myblog/static/media/photocard.e2c24fb0.png"
+  },
+  {
+    "revision": "07f21903302ec4c65e5c2937e3d71127",
+    "url": "/React-myblog/static/media/portrait.07f21903.JPG"
+  },
+  {
+    "revision": "c0d8bde79928c72a0276c3b6f62908a6",
+    "url": "/React-myblog/static/media/postcard.c0d8bde7.jpg"
+  },
+  {
+    "revision": "d75ee55c9f463b8804b1128b6d257e3e",
+    "url": "/React-myblog/static/media/sidePortrait.d75ee55c.jpg"
+  }
+]);
